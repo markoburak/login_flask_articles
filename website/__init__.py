3a970b3ad842249d27a4f41ee9ac4f543c4f697a
markoburak <burakmarko@gmail.com>
2023-04-29 17:13:28 -0400

    tweaked app for heroku

--- website/__init__.py
+++ website/__init__.py
@@ -10,7 +10,9 @@ DB_NAME = "database_login.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "asdfghkl;"
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"postgres://pzbivfoqweowud:605f03fb62c332436f18a481c0cb20fb8d9816f927a5d180f196e1992ee88018@ec2-3-234-204-26.compute-1.amazonaws.com:5432/d2jouh3ihigios"
     db.init_app(app)
 
     from .views import views
